4296022f742f59fd309c14909c524b20d97319ab
Amitai <git@amitai.ga>
2024-10-09 21:13:03 +0100

    add content populator

--- _onCodeStart.ts
+++ _onCodeStart.ts
@@ -1,1 +1,10 @@
-player.say("hello world")
+let content = [
+    "01-diggy-c",
+    "02-superbow-c",
+    "03-nuke-c",
+    "04-flowerpower-c",
+    "05-panda-c",
+    "06-stripmine-c"
+]
+
+player.execute(`scriptevent tcz:populate ${content.join(" ")}`)
